75d5aef244b13e9dac9cb90ea67d2e65103b2bcc
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2025-07-14 00:48:11 -0500

    solucionando errores en nginx.conf

--- src/components/country-login/services/patient/apollo-auth-patient.ts
+++ src/components/country-login/services/patient/apollo-auth-patient.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const authLink = createHttpLink({
-  uri: 'backend-country.azurewebsites.net/api/v1/graphql',
+  uri: 'https://backend-country.azurewebsites.net/api/v1/graphql',
 });
 
 const authClient = new ApolloClient({
