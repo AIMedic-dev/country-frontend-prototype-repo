d0e8a1266d1b63cd90baa3ce53117f97952c7e75
CarlosCuadrado-24 <carloscuadradot24@gmail.com>
2025-07-13 23:36:44 -0500

    agregando url back

--- src/components/country-login/services/patient/apollo-auth-patient.ts
+++ src/components/country-login/services/patient/apollo-auth-patient.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const authLink = createHttpLink({
-  uri: 'http://localhost:8000/api/v1/graphql',
+  uri: 'backend-country.azurewebsites.net/api/v1/graphql',
 });
 
 const authClient = new ApolloClient({
